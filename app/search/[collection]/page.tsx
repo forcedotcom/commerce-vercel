c479cf47f0d88ae8d88780d0a2cdeb39d0b77a15
Vishal Biradar <vbiradar@salesforce.com>
2025-03-31 00:44:12 +0530

    fixed Category Search Page

--- app/search/[collection]/page.tsx
+++ app/search/[collection]/page.tsx
@@ -6,20 +6,20 @@ import Grid from 'components/grid';
 import ProductGridItems from 'components/layout/product-grid-items';
 import { defaultSort, sorting } from 'lib/constants';
 
-export async function generateMetadata(props: {
-  params: Promise<{ collection: string }>;
-}): Promise<Metadata> {
-  const params = await props.params;
-  const collection = await getCollection(params.collection);
+// export async function generateMetadata(props: {
+//   params: Promise<{ collection: string }>;
+// }): Promise<Metadata> {
+//   const params = await props.params;
+//   const collection = await getCollection(params.collection);
 
-  if (!collection) return notFound();
+//   if (!collection) return notFound();
 
-  return {
-    title: collection.seo?.title || collection.title,
-    description:
-      collection.seo?.description || collection.description || `${collection.title} products`
-  };
-}
+//   return {
+//     title: collection.seo?.title || collection.title,
+//     description:
+//       collection.seo?.description || collection.description || `${collection.title} products`
+//   };
+// }
 
 export default async function CategoryPage(props: {
   params: Promise<{ collection: string }>;
