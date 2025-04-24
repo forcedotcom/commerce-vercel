a16fc917760547aa12d919e4fd72071dbdec84d4
Vishal Biradar <vbiradar@salesforce.com>
2025-04-24 16:04:36 +0530

    Fixed Image issue and update next version

--- components/grid/tile.tsx
+++ components/grid/tile.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
 import Image from 'next/image';
+import Label from '../label';
 
 export function GridTileImage({
   isInteractive = true,
@@ -33,6 +34,14 @@ export function GridTileImage({
             'transition duration-300 ease-in-out group-hover:scale-105': isInteractive
           })}
           {...props}
+        />
+      ) : null}
+      {label ? (
+        <Label
+          title={label.title}
+          amount={label.amount}
+          currencyCode={label.currencyCode}
+          position={label.position}
         />
       ) : null}
     </div>
